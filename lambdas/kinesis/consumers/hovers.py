65b27b84947b82fd1f68e3845e638ce6c16ce08b
Juan Roldán <juanroldan1989@gmail.com>
2024-03-04 13:20:52 +0100

    Kinesis consumers for `clicks` and `hovers` setup

--- lambdas/kinesis/consumers/hovers.py
+++ lambdas/kinesis/consumers/hovers.py
@@ -1,1 +1,17 @@
-# consumer subscribes to 'hovers' stream and writes to 'hovers' table
+import base64
+
+def handler(event, context):
+  try:
+    for record in event['Records']:
+      payload = record['kinesis']
+      message = base64.b64decode(payload['data']).decode('utf-8')
+
+      print(f"HOVERS consumer - Kinesis Message:\n"
+          f"  partition key: {payload['partitionKey']}\n"
+          f"  sequence number: {payload['sequenceNumber']}\n"
+          f"  kinesis schema version: {payload['kinesisSchemaVersion']}\n"
+          f"  data: {message}\n")
+
+      # Do something
+  except Exception as error:
+    print(error)

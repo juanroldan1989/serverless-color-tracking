a76d67e38aa41bc179cebb8ad86dea4f41512bf7
Juan Roldán <juanroldan1989@gmail.com>
2024-03-04 22:24:10 +0100

    `frontend` folder added with static website
    - `polling` AJAX requests setup to display `Clicks` and `Hovers` dashboards

---
+++ frontend/js/events/clicks_and_hovers.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+  // Display "hover" effect on table cell colors
+  $("td")
+    .mouseenter(function() {
+      var color = $(this).attr("class");
+      var hoverColor = "dark" + color;
+      $(this).css("background-color", hoverColor);
+    })
+    .mouseleave(function() {
+      var color = $(this).attr("class");
+      $(this).css("background-color", color);
+    });
+
+  // Trigger "hover" event
+  $("td").mouseenter(function(){
+    var color = this.className;
+
+    $.ajax({
+      type: "POST",
+      url: "https://bc75ggsz07.execute-api.us-east-1.amazonaws.com/dev/v1/events",
+      data: `{"action_color": { "action_name" : "hover", "color_name" : "${color}" } }`
+    });
+  });
+
+  // Trigger "click" event
+  $("td").click(function(){
+    var color = this.className;
+
+    $.ajax({
+      type: "POST",
+      url: "https://bc75ggsz07.execute-api.us-east-1.amazonaws.com/dev/v1/events",
+      data: `{"action_color": { "action_name" : "click", "color_name" : "${color}" } }`
+    });
+  });
+});

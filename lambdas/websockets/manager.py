6a34dcc30cf51fa7fb9ee3f684ea0635f63df970
Juan Roldán <juanroldan1989@gmail.com>
2024-03-11 17:46:40 +0100

    Backend lambda functions re-organized properly

---
+++ lambdas/websockets/manager.py
@@ -0,0 +1,53 @@
+import json
+import boto3
+import os
+
+CONNECTIONS_TABLE = os.environ.get('CONNECTIONS_TABLE')
+STATS_TABLE = os.environ.get('STATS_TABLE')
+
+dynamodb_client = boto3.client('dynamodb')
+
+def handler(event, context):
+  print("event: ", event)
+
+  connectionId = event.get("requestContext", {}).get("connectionId")
+
+  print("connectionId: ", connectionId)
+
+  try:
+    if event["requestContext"]["eventType"] == "CONNECT":
+      dynamodb_client.put_item(
+        TableName=CONNECTIONS_TABLE,
+        Item={'ConnectionId': { 'S': connectionId } }
+      )
+
+      return {
+        "statusCode": 200,
+        "body": "Connected."
+      }
+
+    if event["requestContext"]["eventType"] == "DISCONNECT":
+      dynamodb_client.delete_item(
+        TableName=CONNECTIONS_TABLE,
+        Key={'ConnectionId': { 'S': connectionId } }
+      )
+
+      return {
+        "statusCode": 200,
+        "body": "Disconnected."
+      }
+
+    else:
+      return {
+        "statusCode": 500,
+        "body": "Unrecognized event type."
+      }
+
+  except Exception as error:
+    print(error)
+    return {
+      'statusCode': 500,
+      'body': json.dumps({
+        'message': str(error)
+      })
+    }

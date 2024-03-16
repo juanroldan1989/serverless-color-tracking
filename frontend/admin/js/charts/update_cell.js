33e09eaca02bcefa5254ba376d685191eb2deaf0
Juan Roldán <juanroldan1989@gmail.com>
2024-03-16 21:35:11 +0100

    Frontend - Admin stats populated via Polling mechanism

---
+++ frontend/admin/js/charts/update_cell.js
@@ -0,0 +1,27 @@
+function updateCell(api_key, action, color, count) {
+  var cell_id = "#" + api_key + "_" + action + "_" + color;
+
+  console.log("cell_id: ", cell_id);
+
+  if ($(cell_id).length) {
+    $(cell_id).text(count);
+  } else {
+    console.log("cell_id not found: ", cell_id);
+    console.log("creating row for: ", cell_id);
+
+    var new_row = "<tr>"; // api_key, action, color, count
+    new_row += "<td>" + api_key + "</td>";
+    new_row += "<td>" + action + "</td>";
+    new_row += "<td>" + color + "</td>";
+    new_row += "<td id='" + api_key + "_" + action + "_" + color + "'>" + count + "</td>";
+    new_row += "</tr>";
+
+    console.log("new_row: ", new_row);
+
+    var table_body = $("#stats tbody");
+
+    $(table_body).append(new_row);
+
+    console.log("new row added for: ", cell_id);
+  }
+};
---
+++ frontend/admin/js/polling/draw_stats_table.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  setInterval(function() {
+    $.ajax({
+      type: "GET",
+      url: "https://qqoj6um6g8.execute-api.us-east-1.amazonaws.com/dev/admin/v1/stats",
+      // beforeSend: function (xhr) {
+      //   xhr.setRequestHeader("Authorization", "api_key");
+      // },
+      success: function(data){
+        console.log("admin - draw table - data: ", data);
+        $.each( data.stats, function( key, data ) {
+          console.log("admin - draw table - data: ", data);
+          // data = {
+          //   "api_key" : "api_key",
+          //   "action" : "hover",
+          //   "counts" : [
+          //     {'action': 'hover', 'color': 'blue', 'count': 317},
+          //     {'action': 'hover', 'color': 'green', 'count': 308},
+          //     {'action': 'hover', 'color': 'yellow', 'count': 286},
+          //     {'action': 'hover', 'color': 'red', 'count': 308}
+          //   ]
+          // }
+
+          var api_key = data.api_key;
+          var action = data.action;
+          var counts = data.counts;
+
+          $.each(counts, function(key, val) {
+            var color = val.color;
+            var count = val.count;
+
+            updateCell(api_key, action, color, count);
+          });
+        });
+      }
+    });
+  }, 500);
+});

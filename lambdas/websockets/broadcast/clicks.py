715686b9387c8f2d3f2fbada58f26fac76069c08
Juan Roldán <juanroldan1989@gmail.com>
2024-03-16 15:58:20 +0100

    Backend - broadcasting lambda functions checking for API_KEY always

--- lambdas/websockets/broadcast/clicks.py
+++ lambdas/websockets/broadcast/clicks.py
@@ -1,24 +1,31 @@
 import json
+from lambdas.common.extractUtils import extract_payload
 from lambdas.websockets.broadcast.common.connectionUtils import get_connections, send_to_connection
 from lambdas.common.statsUtils import format_stats, get_stats
 
 def handler(event, context):
   print("broadcast_stats event: ", event)
 
-  # TODO: use api_key from kinesis event similar as in consumers logic.
-
   try:
-    connections = get_connections('api_key')
-    data = get_stats('api_key', 'click')
-    stats = format_stats(data, 'click')
+    for record in event['Records']:
+      payload = extract_payload(record)
 
-    print("stats: ", stats)
-    print("connections['Items']: ", connections["Items"])
+      print("payload: ", payload)
 
-    for connection in connections["Items"]:
-      print("connection in loop: ", connection)
-      data = { 'stats': stats, 'event_type': 'click' }
-      send_to_connection(connection, data)
+      if payload['api_key'] is None:
+        continue
+
+      connections = get_connections(payload['api_key'])
+      data = get_stats(payload['api_key'], 'click')
+      stats = format_stats(data, 'click')
+
+      print("stats: ", stats)
+      print("connections['Items']: ", connections["Items"])
+
+      for connection in connections["Items"]:
+        print("connection in loop: ", connection)
+        data = { 'stats': stats, 'event_type': 'click' }
+        send_to_connection(connection, data)
 
   except Exception as error:
     print('error: ', error)

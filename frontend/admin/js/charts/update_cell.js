957d8cfa91038abe4f62f29bc7e696ee401677eb
Juan Roldán <juanroldan1989@gmail.com>
2024-03-17 00:04:12 +0100

    Admin Stats page - Colors added as background within Count cells

--- frontend/admin/js/charts/update_cell.js
+++ frontend/admin/js/charts/update_cell.js
@@ -9,11 +9,10 @@ function updateCell(api_key, action, color, count) {
     console.log("cell_id not found: ", cell_id);
     console.log("creating row for: ", cell_id);
 
-    var new_row = "<tr>"; // api_key, action, color, count
+    var new_row = "<tr>"; // api_key, action, count
     new_row += "<td>" + api_key + "</td>";
     new_row += "<td>" + action + "</td>";
-    new_row += "<td>" + color + "</td>";
-    new_row += "<td id='" + api_key + "_" + action + "_" + color + "'>" + count + "</td>";
+    new_row += `<td style='background-color: ${color} !important' id='` + api_key + "_" + action + "_" + color + "'>" + count + "</td>";
     new_row += "</tr>";
 
     console.log("new_row: ", new_row);
